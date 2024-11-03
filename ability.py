53b5cc3530aa320694e8658027ca91e3cfb2ea7d
JDScript0117 <jdscript0117@gmail.com>
2024-11-03 16:22:47 +0800

    Update ability.py

--- ability.py
+++ ability.py
@@ -1,7 +1,10 @@
+from score import *
+
 class Ability:
     k: float = 0
     lmt: float = 0
-    abilities: {str: int} = {}
+    value: float = 0
+    abilities: {str: Score} = {}
     keys: [str] = []
     def __init__(self) -> None:
         self.k = 0
@@ -9,26 +12,26 @@ class Ability:
         self.keys = ["Search", "DP", "String", "Math", "DS", "Graph", "Geometry", "Misc"]
         self.abilities = {}
         for key in self.keys:
-            self.abilities[key] = 0
+            self.abilities[key] = Score()
+        return
+    def updateValue(self) -> None:
+        sumabilities: int = 0
+        sumpowerdiff: float = 0
+        for key in self.keys:
+            sumabilities += self.abilities[key].score
+            if self.abilities[key].score < self.lmt:
+                sumpowerdiff -= (self.abilities[key].score - self.lmt) ** 2
+            else:
+                sumpowerdiff += (self.abilities[key].score - self.lmt) ** 2
+        self.value = self.k * sumabilities + sumpowerdiff
         return
     def update(self, keys: [str] = [], diff: [int] = []) -> bool:
         flag: bool = False
         for ind in range(len(keys)):
             key: int = keys[ind]
             if key in self.keys:
-                self.abilities[key] += diff[ind]
-                if self.abilities[key] < 0:
-                    self.abilities[key] = 0
+                self.abilities[key].update(diff[ind])
             else:
                 flag = True
+        self.updateValue()
         return flag
-    def getValue(self) -> float:
-        sumabilities: int = 0
-        sumpowerdiff: float = 0
-        for key in self.keys:
-            sumabilities += self.abilities[key]
-            if self.abilities[key] < self.lmt:
-                sumpowerdiff -= (self.abilities[key] - self.lmt) ** 2
-            else:
-                sumpowerdiff += (self.abilities[key] - self.lmt) ** 2
-        return self.k * sumabilities + sumpowerdiff
