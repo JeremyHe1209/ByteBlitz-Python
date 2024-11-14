7d3ff1d66afd807f734726d6efbde63ae36bd35d
JDScript0117 <jdscript0117@gmail.com>
2024-11-14 21:30:10 +0800

    Update score.py

--- score.py
+++ score.py
@@ -1,12 +1,18 @@
+from ahpair import *
+
 class Score:
     score: int = 0
     grade: int = 0
     scoretograde: [int] = []
-    gradetobuff: [[int]] = {}
-    def __init__(self) -> None:
+    gradetobuff: [AHPair] = {}
+    name: str = ""
+    buffname: str = ""
+    def __init__(self, name: str) -> None:
         self.score = 0
         self.grade = 0
         self.scoretograde = []
+        self.name = name
+        self.buffname = name + "temratebuff"
         for i in range(3):
             self.scoretograde.append(0)
         for i in range(2):
@@ -18,10 +24,16 @@ class Score:
         for i in range(5):
             self.scoretograde.append(4)
         self.gradetobuff = [
-            [0, 0], [1, 0], [2, 1],
-            [3, 2], [4, 4], [6, 6]
+            AHPair(0, 0, True, self.buffname),
+            AHPair(1, 0, True, self.buffname),
+            AHPair(2, 1, True, self.buffname),
+            AHPair(3, 2, True, self.buffname),
+            AHPair(4, 4, True, self.buffname),
+            AHPair(6, 6, True, self.buffname)
         ]
         return
+    def getBuff(self) -> AHPair:
+        return self.gradetobuff[self.grade]
     def updateGrade(self) -> None:
         if self.score >= 15:
             self.grade = 5
