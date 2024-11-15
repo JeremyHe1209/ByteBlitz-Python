7cf230d5e9fd678e668317e1ac60a371981da4f0
JeremyHe1209 <heyicong.jeremy@gmail.com>
2024-11-15 18:57:48 +0800

    Update ability.py

--- ability.py
+++ ability.py
@@ -2,15 +2,17 @@ from score import *
 
 class Ability:
     k: float = 0
-    lmt: float = 0
+    limit: float = 0
     value: float = 0
     abilities: {str: Score} = {}
     keys: [str] = []
+    buff: Buff = Buff(0, 0)
     def __init__(self) -> None:
         self.k = 0
-        self.lmt = 0
+        self.limit = 0
         self.keys = ["Search", "DP", "String", "Math", "DS", "Graph", "Geometry", "Misc"]
         self.abilities = {}
+        self.buff = Buff(0, 0)
         for key in self.keys:
             self.abilities[key] = Score(key)
         return
@@ -19,19 +21,24 @@ class Ability:
         sumpowerdiff: float = 0
         for key in self.keys:
             sumabilities += self.abilities[key].score
-            if self.abilities[key].score < self.lmt:
-                sumpowerdiff -= (self.abilities[key].score - self.lmt) ** 2
+            if self.abilities[key].score < self.limit:
+                sumpowerdiff -= (self.abilities[key].score - self.limit) ** 2
             else:
-                sumpowerdiff += (self.abilities[key].score - self.lmt) ** 2
+                sumpowerdiff += (self.abilities[key].score - self.limit) ** 2
         self.value = self.k * sumabilities + sumpowerdiff
         return
-    def update(self, keys: [str] = [], diff: [int] = []) -> bool:
+    def updateBuff(self) -> None:
+        self.buff = Buff(0, 0)
+        for key in self.keys:
+            self.buff += self.abilities[key].getBuff()
+        return
+    def update(self, abilities: {str : int}) -> bool:
         flag: bool = False
-        for ind in range(len(keys)):
-            key: int = keys[ind]
-            if key in self.keys:
-                self.abilities[key].update(diff[ind])
-            else:
+        for key in abilities.keys():
+            if key not in self.keys:
                 flag = True
-        self.updateValue()
+            else:
+                self.abilities[key].update(abilities[key])
+        updateValue()
+        updateBuff()
         return flag
