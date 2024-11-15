81451530654bc15a610d81592fd2b24a054ae9b9
JeremyHe1209 <heyicong.jeremy@gmail.com>
2024-11-15 19:00:40 +0800

    Update score.py

--- score.py
+++ score.py
@@ -1,43 +1,41 @@
-from ahpair import *
+from buff import *
 
 class Score:
     score: int = 0
     grade: int = 0
     scoretograde: [int] = []
-    gradetobuff: [AHPair] = {}
+    gradetobuff: [Buff] = {}
     name: str = ""
-    buffname: str = ""
-    def __init__(self, name: str) -> None:
+    def __init__(self, name) -> None:
         self.score = 0
         self.grade = 0
-        self.scoretograde = []
         self.name = name
-        self.buffname = name + "temratebuff"
-        for i in range(3):
+        self.scoretograde = []
+        for i in range(2):
             self.scoretograde.append(0)
         for i in range(2):
             self.scoretograde.append(1)
-        for i in range(2):
+        for i in range(3):
             self.scoretograde.append(2)
         for i in range(3):
             self.scoretograde.append(3)
         for i in range(5):
             self.scoretograde.append(4)
         self.gradetobuff = [
-            AHPair(0, 0, True, self.buffname),
-            AHPair(1, 0, True, self.buffname),
-            AHPair(2, 1, True, self.buffname),
-            AHPair(3, 2, True, self.buffname),
-            AHPair(4, 4, True, self.buffname),
-            AHPair(6, 6, True, self.buffname)
+            Buff(0, 0),
+            Buff(1, 0),
+            Buff(2, 1),
+            Buff(3, 2),
+            Buff(4, 4),
+            Buff(6, 6)
         ]
         return
-    def getBuff(self) -> AHPair:
+    def getBuff(self) -> Buff:
         return self.gradetobuff[self.grade]
     def updateGrade(self) -> None:
         if self.score >= 15:
             self.grade = 5
-        self.grade = self.scoretograde[self.score]
+        self.grade = self.scoretograde[self.grade]
         return
     def update(self, diff: int) -> None:
         self.score += diff
