7cd32406200d278e4f5c723c76de7b5e1f2a3233
JDScript0117 <jdscript0117@gmail.com>
2024-11-15 22:53:14 +0800

    Update ability.py

--- ability.py
+++ ability.py
@@ -2,13 +2,13 @@ from score import *
 
 class Ability:
     k: float = 0
-    lmt: float = 0
+    limit: float = 0
     value: float = 0
     abilities: {str: Score} = {}
     keys: [str] = []
     def __init__(self) -> None:
         self.k = 0
-        self.lmt = 0
+        self.limit = 0
         self.keys = ["Search", "DP", "String", "Math", "DS", "Graph", "Geometry", "Misc"]
         self.abilities = {}
         for key in self.keys:
@@ -19,10 +19,10 @@ class Ability:
         sumpowerdiff: float = 0
         for key in self.keys:
             sumabilities += self.abilities[key].score
-            if self.abilities[key].score < self.lmt:
-                sumpowerdiff -= (self.abilities[key].score - self.lmt) ** 2
+            if self.abilities[key].score < self.limit:
+                sumpowerdiff -= (self.abilities[key].score - self.limit) ** 2
             else:
-                sumpowerdiff += (self.abilities[key].score - self.lmt) ** 2
+                sumpowerdiff += (self.abilities[key].score - self.limit) ** 2
         self.value = self.k * sumabilities + sumpowerdiff
         return
     def update(self, keys: [str] = [], diff: [int] = []) -> bool:
