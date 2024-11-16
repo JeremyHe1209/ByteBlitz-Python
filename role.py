44368c03a8f7b078f5788036a135aa56cf6a5083
JeremyHe1209 <heyicong.jeremy@gmail.com>
2024-11-16 11:17:40 +0800

    Update role.py

--- role.py
+++ role.py
@@ -1,15 +1,15 @@
+from buffcontainer import *
+
 class Role:
     legalattacktimes: int = 0
-    attack: int = 0
-    health: int = 0
+    ahstates: BuffContainer = BuffContainer([])
     roleid: int = 0
     isdead: bool = 0
     keys: [str] = []
     figures: {str : bool} = {}
     def __init__(self, legalattacktimes: int, attack: int, health: int, roleid: int, newfigures: [str]) -> None:
         self.legalattacktimes = legalattacktimes
-        self.attack = attack
-        self.health = health
+        self.ahstates = BuffContainer([Buff(attack, health, False, "initahpair")])
         self.roleid = roleid
         self.isdead = False
         self.keys = ["hero", "defensive", "firewall", "hidden", "debug"]
@@ -19,13 +19,9 @@ class Role:
         self.giveFigures(newfigures)
         return
     def update(self, attackdiff: int, healthdiff: int) -> None:
-        self.attack += attackdiff
-        self.health += healthdiff
-        if self.attack < 0:
-            self.attack = 0
-        if self.health < 0:
-            self.health = 0
-            self.isdead = False
+        self.ahstates.update(attackdiff, healthdiff)
+        if self.ahstates.total.health == 0:
+            self.isdead = True
         return
     def giveFigures(self, newfigures: [str]) -> bool:
         flag: bool = False
