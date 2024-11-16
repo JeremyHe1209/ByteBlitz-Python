42170daf37cbcccc8af10ec7308a9616dc6e70db
JeremyHe1209 <heyicong.jeremy@gmail.com>
2024-11-16 11:18:02 +0800

    Update score.py

--- score.py
+++ score.py
@@ -6,36 +6,36 @@ class Score:
     scoretograde: [int] = []
     gradetobuff: [Buff] = {}
     name: str = ""
-    def __init__(self, name) -> None:
+    buffname: str = ""
+    def __init__(self, name: str) -> None:
         self.score = 0
         self.grade = 0
-        self.name = name
         self.scoretograde = []
-        for i in range(2):
+        self.name = name
+        self.buffname = name + "attacktimetemperatebuff"
+        for i in range(3):
             self.scoretograde.append(0)
         for i in range(2):
             self.scoretograde.append(1)
-        for i in range(3):
+        for i in range(2):
             self.scoretograde.append(2)
         for i in range(3):
             self.scoretograde.append(3)
         for i in range(5):
             self.scoretograde.append(4)
         self.gradetobuff = [
-            Buff(0, 0),
-            Buff(1, 0),
-            Buff(2, 1),
-            Buff(3, 2),
-            Buff(4, 4),
-            Buff(6, 6)
+            Buff(0, 0, True, self.buffname), Buff(1, 0, True, self.buffname),
+            Buff(2, 1, True, self.buffname), Buff(3, 2, True, self.buffname),
+            Buff(4, 4, True, self.buffname), Buff(6, 6, True, self.buffname)
         ]
         return
     def getBuff(self) -> Buff:
-        return self.gradetobuff[self.grade]
+        buff = self.gradetobuff[self.grade]
+        return Buff(buff.attack, buff.health, buff.isTemporary, buff.name)
     def updateGrade(self) -> None:
         if self.score >= 15:
             self.grade = 5
-        self.grade = self.scoretograde[self.grade]
+        self.grade = self.scoretograde[self.score]
         return
     def update(self, diff: int) -> None:
         self.score += diff
