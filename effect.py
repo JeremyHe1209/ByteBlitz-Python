e14ee8025ff4e0b8f1df8a5373b4a0e07a0ff564
heyicong <heyicong.jeremy@gmail.com>
2024-12-20 19:47:59 +0800

    update
    update & fix

--- effect.py
+++ effect.py
@@ -3,27 +3,34 @@ from role import *
 from battle import *
 
 class Effect:
+    pass
+
+class HeroEffect(Effect):
+    name : str
+    isred : bool
+    def __init__(self, name : str, isred : bool) -> None:
+        self.name = name
+        self.isred = isred
+        return
+    def canHappen(self, battle : Battle) -> bool:
+        if self.isred:
+            return battle.red.hero.canBeSelected()
+        else:
+            return battle.blue.hero.canBeSelected()
+
+class MinionsEffect(Effect):
+    name : str
+    isred : bool
+    number : int
+    def __init__(self, name : str, isred : bool, number : int) -> None:
+        self.name = name
+        self.isred = isred
+        self.number = number
+    def canHappen(self, battle : Battle, roles : list[Role]) -> bool:
+        # 检查是否能对指定的角色产生影响的代码应在外部实现；原则上，一定存在一种对角色的选择使其合法。
+        return True
+
+class BattleEffect(Effect):
     name : str
     def __init__(self, name : str) -> None:
         self.name = name
-    def happens(self, battle : Battle) -> None:
-        pass
-
-class RoleEffect(Effect):
-    def canHappen(self, battle : Battle) -> bool:
-        return True
-
-class RedHeroEffect(RoleEffect):
-    def canHappen(self, battle : Battle) -> bool:
-        return battle.red.hero.canBeSelected()
-    def happens(self, battle : Battle) -> None:
-        pass
-
-class BlueHeroEffect(RoleEffect):
-    def canHappen(self, battle : Battle) -> bool:
-        return battle.blue.hero.canBeSelected()
-    def happens(self, battle : Battle) -> None:
-        pass
-
-class RedMinionEffect(RoleEffect):
-    pass
--- minioncontainer.py
+++ minioncontainer.py
@@ -1,4 +1,18 @@
+# MinionContainer 初代代码，仅用于测试effect。待后期完善.
 from classes import *
+from minion import *
 
 class MinionContainer:
-    pass
+    minions : list[Minion]
+    def __init__(self, minions : list[Minion] = []) -> None:
+        self.minions = minions
+        return
+    def add(self, minions : list[Minion]) -> None:
+        self.minions = self.minions + minions
+        return 
+    def delete(self, minions : list[Minion]) -> bool:
+        for minion in minions:
+            if minion in self.minions:
+                self.minions.remove(minion)
+                minions.remove(minion)
+        return len(minions)
