5cf0947ad2a40427dab83d34a6e51a28b473a996
heyicong <heyicong.jeremy@gmail.com>
2024-12-31 22:52:53 +0800

    Update
    Fixed Effect.

--- battle.py
+++ battle.py
@@ -4,7 +4,7 @@ from team import *
 class Battle:
     red : Team # self
     blue : Team
-    def __init__(self) -> None:
-        self.red = Team()
-        self.blue = Team()
+    def __init__(self, redhero : Hero, bluehero : Hero) -> None:
+        self.red = Team(redhero)
+        self.blue = Team(bluehero)
         return
--- effect.py
+++ effect.py
@@ -3,13 +3,19 @@ from role import *
 from battle import *
 
 class Effect:
+    name : str
+    def __init__(self, name : str) -> None:
+        self.name = name
+        return
+    def canHappen(self, battle : Battle) -> bool:
+        return True
     pass
 
 class HeroEffect(Effect):
     name : str
     isred : bool
     def __init__(self, name : str, isred : bool) -> None:
-        self.name = name
+        super(HeroEffect, self).__init__(name)
         self.isred = isred
         return
     def canHappen(self, battle : Battle) -> bool:
@@ -23,9 +29,10 @@ class MinionsEffect(Effect):
     isred : bool
     number : int
     def __init__(self, name : str, isred : bool, number : int) -> None:
-        self.name = name
+        super(MinionsEffect, self).__init__(name)
         self.isred = isred
         self.number = number
+        return
     def canHappen(self, battle : Battle, roles : list[Role]) -> bool:
         # 检查是否能对指定的角色产生影响的代码应在外部实现；原则上，一定存在一种对角色的选择使其合法。
         return True
